e3bb50a4504f53cd39dbfa4c0410b9901c98315d
SunnySparks <josediazt6@outlook.com>
2020-12-10 17:04:43 -0600

    Sorcery authentication complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_filter :require_login, :only [:new, :create, :edit, :update, :destroy]
   include ArticlesHelper
     def index
         @articles = Article.all
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,6 @@
 class TagsController < ApplicationController
+    before_filter :require_login, only: [:destroy]
+    
     def show
         @tag = Tag.find(params[:id])
     end
